19c347d91d9c0c0a1fb4272f88e66317c704c37a
Nikonus <nikhildubay.2005@gmail.com>
2026-02-14 12:26:40 +0530

    feat: update database connection configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,42 +1,41 @@
 import mongoose from "mongoose";
-import { buffer } from "stream/consumers";
 
-
-const MONGODB_URL = process.env.MONGODB_URL !;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
 if (!MONGODB_URL) {
-    throw new Error("MONGODB_URL is not defined in environment variables");
+  throw new Error("MONGODB_URL is not defined in environment variables");
 }
 
-
-let cached = global.mongoose;
+let cached = global.mongoose as {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-
 export async function connectToDatabase() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false,
-            maxpoolsize: 10,
-        };
-
-        cached.promise = mongoose.connect(MONGODB_URL).then(() => mongoose.connection )
-            
-        
-    }
-
-
-    try {
-        cached.conn = await cached.promise;
-        return cached.conn;
-    } catch (error) {
-        throw error;
-    }
+  if (cached.conn) {
     return cached.conn;
-        };
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URL, opts)
+      .then((mongoose) => mongoose);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+}
