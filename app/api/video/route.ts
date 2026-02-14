6abb8664e24a7c78f398cde20d67ccd9eb480ddd
Nikonus <nikhildubay.2005@gmail.com>
2026-02-14 12:27:15 +0530

    feat: add video API routes

---
+++ app/api/video/route.ts
@@ -0,0 +1,68 @@
+
+import { connectToDatabase } from "@/lib/db";
+import Video, { IVideo } from "@/model/video.model";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    try {
+        await connectToDatabase();
+        const videos = await Video.find().sort({ createdAt: -1 }).lean();
+        if (!videos|| videos.length === 0) {
+            return NextResponse.json({ error: "No videos found" }, { status: 404 });
+        }
+        return NextResponse.json(videos);
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to fetch videos" }, { status: 500 });
+    }
+
+  } catch (error) {
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+
+export async function POST(request: Request) {
+  try {
+   const session = await getServerSession();
+   if (!session || !session.user) {
+     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+   }
+
+   await connectToDatabase();
+
+   const body : IVideo = await request.json();
+
+   if (!body.title || !body.description || !body.videoUrl) {
+     return NextResponse.json({ error: "Missing required fields" }, { status: 400 });
+   }
+
+   const videoDate ={
+    ...body,
+    thumbnailUrl: body.thumbnailUrl || "https://ik.imagekit.io/fjgmko3fdz/default-thumbnail.jpg",
+    controls:body.controls?? true,
+    transformations:{
+        crop:body.transformations?.crop?? {
+            x: 0,
+            y: 0,
+            width: 1280,
+            height: 720
+        },
+        resize:body.transformations?.resize?? {
+            width: 1280,
+            height: 720 
+        },
+        rotate: body.transformations?.rotate?? 0
+        
+    }
+   }
+
+   const newVideo = await Video.create(videoDate);
+
+   return NextResponse.json(videoDate, { status: 201 });
+} catch (error) {
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
+ 
